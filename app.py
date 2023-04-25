6f61c5480ceec261a655722f1d275d7bddc7c616
xenaoomka <xenaoomka@gmail.com>
2023-04-25 09:53:30 -0600

    custom login and cleanup

--- app.py
+++ app.py
@@ -1,19 +1,62 @@
 import datetime
-from flask import Flask, render_template, request, redirect, send_from_directory
-from flask_bootstrap import Bootstrap
+from dotenv import load_dotenv
+from flask import Flask, flash, render_template, request, redirect, send_from_directory, url_for
 import sqlite3
 import os
+from flask_login import current_user, login_user
+from flask_security import Security, SQLAlchemyUserDatastore, login_required, UserMixin
+from flask_sqlalchemy import SQLAlchemy
+from flask_security.utils import hash_password
+from flask_bcrypt import Bcrypt, check_password_hash
 import pytz
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
 
-DB_NAME = '/app/content/videoConvert.db'
-# '/Users/oomkaxena/Desktop/content/videoConvert.db'
-# '/app/content/videoConvert.db'
-VIDEO_DIRECTORY = '/app/content/output'
-# '/Users/oomkaxena/Desktop/content/output'
-# '/app/content/output'
+# Load environment variables from .env file
+load_dotenv()
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SECURITY_PASSWORD_SALT'] = os.environ.get('SECURITY_PASSWORD_SALT')
+app.config['SECURITY_REGISTERABLE'] = os.environ.get('SECURITY_REGISTERABLE')
+
+
+db = SQLAlchemy(app)
+
+roles_users = db.Table('roles_users',
+                       db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+                       db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+class Role(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    fs_uniquifier = db.Column(db.String(64), unique=True)
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
+
+# Create the database tables
+db.create_all()
+
+# Create a UserDatastore object
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+
+# Initialize the Flask-Security extension
+security = Security(app, user_datastore)
+
+# Create a new user and hash their password
+with app.app_context():
+    if not User.query.filter_by(email=os.environ.get('USER_EMAIL')).first():
+        email = os.environ.get('USER_EMAIL')
+        password = os.environ.get('USER_PASSWORD')
+        user_datastore.create_user(email=email, password=hash_password(password))
+        db.session.commit()
 
 # Convert UTC timestamp to MST
 def convert_timestamp(timestamp):
@@ -24,11 +67,12 @@ def convert_timestamp(timestamp):
     return mountain_timestamp.strftime('%m/%d/%Y %I:%M %p')
 
 @app.route('/')
-def index():
+@login_required
+def home():
     # Set the timezone to Mountain Time
     mountain_tz = pytz.timezone('US/Mountain')
     # Fetch all records from the database
-    conn = sqlite3.connect(DB_NAME)
+    conn = sqlite3.connect(os.getenv('DB_NAME'))
     c = conn.cursor()
     c.execute('SELECT id, original_file_name, original_file_date, converted_file_name, converted_file_date, claim_number, ad_notes FROM converted_videos')
     videos = c.fetchall()
@@ -49,12 +93,12 @@ def index():
 
     conn.close()
 
-    return render_template('index.html', videos=videos, datetime=datetime)
+    return render_template('home.html', videos=videos, datetime=datetime)
 
 @app.route('/update', methods=['POST'])
 def update():
     # Update record in the database
-    conn = sqlite3.connect(DB_NAME)
+    conn = sqlite3.connect(os.environ.get('DB_NAME'))
     c = conn.cursor()
     file_id = request.form.get('id')
     claim_number = request.form.get('claim_number')
@@ -72,8 +116,8 @@ def play_video():
 def video(filename):
     """Route to serve video files."""
     # Check if the requested file exists in the video directory
-    if filename in os.listdir(VIDEO_DIRECTORY):
-        return send_from_directory(VIDEO_DIRECTORY, filename)
+    if filename in os.listdir(os.environ.get('VIDEO_DIRECTORY')):
+        return send_from_directory(os.environ.get('VIDEO_DIRECTORY'), filename)
     else:
         return "Video not found", 404
 
@@ -81,7 +125,7 @@ def video(filename):
 def play(filename):
     """Route to play video files in a separate template page."""
     # Check if the requested file exists in the video directory
-    if filename in os.listdir(VIDEO_DIRECTORY):
+    if filename in os.listdir(os.environ.get('VIDEO_DIRECTORY')):
         return render_template('play.html', video_filename=filename)
     else:
         return "Video not found", 404
